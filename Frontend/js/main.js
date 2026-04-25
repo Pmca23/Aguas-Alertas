9fd8dcb532adef51c02cde9ab34fd218f424ed85
luisquirin <lhgquirin@gmail.com>
2026-04-25 19:30:55 +0100

    Add frontend completo com dashboard

---
+++ Frontend/js/main.js
@@ -0,0 +1,5 @@
+console.log("Frontend Águas Alerta ligado!");
+
+document.getElementById("distancia").textContent = "15 cm";
+document.getElementById("temperatura").textContent = "22 °C";
+document.getElementById("chuva").textContent = "Sem chuva";
